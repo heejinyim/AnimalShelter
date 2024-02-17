b67d11e47281a047333a97347c57cc17a9f0d57b
Huijin Yim <152468548+heejinyim@users.noreply.github.com>
2024-02-17 12:50:58 -0600

    Add files via upload

---
+++ AnimalShelter/AnimalShelter/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace AnimalShelter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreateCustomer = new System.Windows.Forms.Button();
+            this.CusDetailPanel = new System.Windows.Forms.Panel();
+            this.CusIsQualified = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CusDescription = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.CusAddress = new System.Windows.Forms.Label();
+            this.CusAge = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.CusFullName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CusDetailPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CreateCustomer
+            // 
+            this.CreateCustomer.Location = new System.Drawing.Point(60, 43);
+            this.CreateCustomer.Name = "CreateCustomer";
+            this.CreateCustomer.Size = new System.Drawing.Size(168, 48);
+            this.CreateCustomer.TabIndex = 0;
+            this.CreateCustomer.Text = "입양자 생성";
+            this.CreateCustomer.UseVisualStyleBackColor = true;
+            this.CreateCustomer.Click += new System.EventHandler(this.CreateCustomer_Click);
+            // 
+            // CusDetailPanel
+            // 
+            this.CusDetailPanel.Controls.Add(this.CusIsQualified);
+            this.CusDetailPanel.Controls.Add(this.label3);
+            this.CusDetailPanel.Controls.Add(this.CusDescription);
+            this.CusDetailPanel.Controls.Add(this.label8);
+            this.CusDetailPanel.Controls.Add(this.CusAddress);
+            this.CusDetailPanel.Controls.Add(this.CusAge);
+            this.CusDetailPanel.Controls.Add(this.label5);
+            this.CusDetailPanel.Controls.Add(this.label6);
+            this.CusDetailPanel.Controls.Add(this.CusFullName);
+            this.CusDetailPanel.Controls.Add(this.label2);
+            this.CusDetailPanel.Location = new System.Drawing.Point(293, 28);
+            this.CusDetailPanel.Name = "CusDetailPanel";
+            this.CusDetailPanel.Size = new System.Drawing.Size(450, 391);
+            this.CusDetailPanel.TabIndex = 1;
+            // 
+            // CusIsQualified
+            // 
+            this.CusIsQualified.AutoSize = true;
+            this.CusIsQualified.Location = new System.Drawing.Point(167, 183);
+            this.CusIsQualified.Name = "CusIsQualified";
+            this.CusIsQualified.Size = new System.Drawing.Size(0, 20);
+            this.CusIsQualified.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Qualification:";
+            // 
+            // CusDescription
+            // 
+            this.CusDescription.AutoSize = true;
+            this.CusDescription.Location = new System.Drawing.Point(167, 236);
+            this.CusDescription.Name = "CusDescription";
+            this.CusDescription.Size = new System.Drawing.Size(0, 20);
+            this.CusDescription.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(35, 236);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(93, 20);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Description:";
+            // 
+            // CusAddress
+            // 
+            this.CusAddress.AutoSize = true;
+            this.CusAddress.Location = new System.Drawing.Point(167, 135);
+            this.CusAddress.Name = "CusAddress";
+            this.CusAddress.Size = new System.Drawing.Size(0, 20);
+            this.CusAddress.TabIndex = 7;
+            // 
+            // CusAge
+            // 
+            this.CusAge.AutoSize = true;
+            this.CusAge.Location = new System.Drawing.Point(167, 86);
+            this.CusAge.Name = "CusAge";
+            this.CusAge.Size = new System.Drawing.Size(0, 20);
+            this.CusAge.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Address:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(35, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 20);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Age:";
+            // 
+            // CusFullName
+            // 
+            this.CusFullName.AutoSize = true;
+            this.CusFullName.Location = new System.Drawing.Point(165, 43);
+            this.CusFullName.Name = "CusFullName";
+            this.CusFullName.Size = new System.Drawing.Size(0, 20);
+            this.CusFullName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Full Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CusDetailPanel);
+            this.Controls.Add(this.CreateCustomer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.CusDetailPanel.ResumeLayout(false);
+            this.CusDetailPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateCustomer;
+        private System.Windows.Forms.Panel CusDetailPanel;
+        private System.Windows.Forms.Label CusAddress;
+        private System.Windows.Forms.Label CusAge;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label CusFullName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label CusDescription;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label CusIsQualified;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AnimalShelter/AnimalShelter/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimalShelter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateCustomer_Click(object sender, EventArgs e)
+        {
+            Customer cus = new Customer("Sujin", "Lim", new DateTime(1995, 8, 4));
+            cus.Address = "123 Wilhire Blvd.";
+            cus.Description = "She wants to adopt a dog.";
+
+            CusFullName.Text = cus.FullName;
+            CusAge.Text = cus.Age.ToString();
+            CusAddress.Text = cus.Address;
+            CusIsQualified.Text = cus.IsQualified.ToString();
+            CusDescription.Text = cus.Description;
+        }
+    }
+}
